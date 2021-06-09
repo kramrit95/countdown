0519b6af7ac0e343e553d3ef390055b26ae45842
Amrit kumar <79562154+kramrit95@users.noreply.github.com>
2021-06-09 15:39:26 +0530

    Create .cpp

---
+++ .cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+
+
+int n;
+
+cin>>n;
+for(int i=n;i>0;i--)
+{
+if(i%5==0)
+{
+cout<<i<<endl;
+    cout<<"Beep"<<endl;}
+
+else
+
+   cout<<i<<endl;
+    
+}
+
+    return 0;
+}    
+    
